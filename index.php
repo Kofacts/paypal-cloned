2c363284087a27f07d1d4fabf5c7dbcf36cbf413
Kofacts <obodugorapheal@gmail.com>
2016-11-17 22:36:13 -1100

    Added some images

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 
 /** 
 	Project Name: Paypal Website Designed Cloned
-	Author : Obodugo Prince Rapheal (Kofacts)
-	Date : 18 Nov. 2016 9:35
+	Author : 	  Obodugo Prince Rapheal (Kofacts)
+	Date : 		  18 Nov. 2016 9:35
 
-**/	
+**/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Paypal Website Cloned!</title>
+</head>
+<body>
+
+</body>
+</html>
