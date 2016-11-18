47c1c4489169abd4fc949371a7955541e68b3260
Kofacts <obodugorapheal@gmail.com>
2016-11-17 23:12:20 -1100

    Loaded Style Sheets and Images

--- index.php
+++ index.php
@@ -1,18 +1,74 @@
 <?php
 
-/** 
+/** ------------------------------------------------
 	Project Name: Paypal Website Designed Cloned
 	Author : 	  Obodugo Prince Rapheal (Kofacts)
 	Date : 		  18 Nov. 2016 9:35
-
+	Disclaimer:   The Sole aim of this Project is to get my hands dirty with frontend, 
+				  Nothing else other than this.
+	Technologies: Bootstrap CSS Framework.
+	------------------------------------------------
 **/
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8" http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Paypal Website Cloned!</title>
+	<!-- Link all stylesheets and JS files -->
+	<link rel="stylesheet" type="text/css" href="./assets/inc/bs/css/bootstrap.css">
+	<script type="text/javascript" src="./assets/inc/bs/js/bootstrap.js"></script>
+
+
+	<!-- Beginning of Header Section for Navigation -->
 </head>
+	<section style="background:white; color:white; height:54px; font-family: 'Roboto';">
+		<header class="navbar navbar-default" style="background:transparent; border:none; border-radius:none; color:white; margin-bottom:0px;">
+			<div class="container" style="background-color:white;">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle Navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#" style="color:black; font-family:'Satisfy';">
+					Gupa Network
+						<!-- This would be in a row -->
+						<div class="row">
+							<div class="col-sm-3">
+							<!-- This is the logo -->
+							</div>
+
+							<div class="col-sm-3">
+							<!-- This the the nav close to it -->
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right" id="hoverme">
+						<li class="dropdown is-active">
+							<a class="dropdown-toggle chu" href="#" style="font-family: 'Lato';">Home</a>
+						<li>
+
+						<li>
+								<a class="dropdown-toggle" href="#">About Gupa</a>
+						</li>
+
+						<li>
+								<a class="dropdown-toggle" href="#">Contact Us</a>
+						</li>
+
+						</ul>
+					</div>
+			</div>
+
+		</header>
+	
+	</section>
+
 <body>
 
 </body>
